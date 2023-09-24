98549f6a74cfec381404d0057536097e92802418
Roman <popov.roman.e@gmail.com>
2023-09-25 00:09:35 +0500

    делает появление модального окна

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <button @click="createPermission">create</button>
+
+</template>
+
+<script>
+    import API from "@/api/api";
+    import { mapActions } from 'vuex';
+
+    export default {
+        name: 'Home',
+        data() {
+            return {
+                modalData: null,
+            }
+        },
+        methods: {
+            ...mapActions(['modalAdd']),
+            getTemplates() {
+                API.getTemplates().then((data) => this.modalData = data)
+            },
+            createPermission() {
+                API.createPermission();
+                this.modalAdd(this.modalData);
+            }
+        },
+        mounted() {
+            this.getTemplates()
+        }
+    }
+</script>
